84e0dcc905d961d1b98707c19b00b210c6207586
onder7 <onder7@gmail.com>
2026-08-05 22:45:03 +0300

    Fix crypto.randomUUID polyfill - don't reassign read-only window.crypto

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,8 +6,7 @@ import { initAnalytics } from './lib/analytics'
 
 // Polyfill for crypto.randomUUID on browsers that don't support it
 if (typeof window !== 'undefined' && !window.crypto?.randomUUID) {
-  window.crypto = window.crypto || {} as any;
-  (window.crypto as any).randomUUID = function() {
+  window.crypto.randomUUID = function() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       const r = Math.random() * 16 | 0;
       const v = c === 'x' ? r : (r & 0x3 | 0x8);
